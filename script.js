c4f2265bca06d5dc6f944decd63969de1e048587
rnaka <rae.loves@gmail.com>
2020-09-26 22:36:08 -1000

    Update script.js

--- script.js
+++ script.js
@@ -303,7 +303,7 @@ var censusLayout = {
 
 Plotly.newPlot('pieDiv2', censusData, censusLayout);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([51.5135, -0.136], 17);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
